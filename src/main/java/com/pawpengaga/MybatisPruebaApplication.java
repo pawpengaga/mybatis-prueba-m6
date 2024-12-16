fc0939edaf775bf24379fc1cf0dceed22daef16a
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-16 19:25:25 -0300

    primer commit

---
+++ src/main/java/com/pawpengaga/MybatisPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.pawpengaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawpengaga/MybatisPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawpengaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
